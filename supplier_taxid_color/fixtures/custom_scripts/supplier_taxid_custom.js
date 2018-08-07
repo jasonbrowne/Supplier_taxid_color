51f4cce7a89e5a77db443814be73f03b0dffd9a1
jason <jason@ixsystems.com>
2018-08-07 11:20:43 -0700

    2nd commit

--- supplier_taxid_color/fixtures/custom_scripts/supplier_taxid_custom.js
+++ supplier_taxid_color/fixtures/custom_scripts/supplier_taxid_custom.js
@@ -2,7 +2,7 @@
 // change color of txt field if not on list 
 
 frappe.ui.form.on("Supplier", "validate", function(frm) {
-   if (frm.doc.tax_id != 'frm.doc.supplier_taxid_prefix' ()) {
+   if (frm.doc.tax_id !== 'frm.doc.supplier_taxid_prefix' ()) {
 frappe.throw("<b>Please enter tax id for supplier</b>");
        validated = false;
         }

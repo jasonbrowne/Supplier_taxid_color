2ba751fbd91dd6808cb7e1d0da3337fe6ee068bc
jason <jason@ixsystems.com>
2018-08-09 11:12:24 -0700

    fix syntex error

--- supplier_taxid_color/fixtures/custom_scripts/supplier_taxid.py
+++ supplier_taxid_color/fixtures/custom_scripts/supplier_taxid.py
@@ -1,5 +1,5 @@
-// validate Supplier taxid prefix aginst list from irs 
-// change color of txt field if not on list 
+# validate Supplier taxid prefix aginst list from irs 
+# change color of txt field if not on list 
 
 frappe.ui.form.on("Supplier", "validate", function(frm,cdt,cdn) {
    if (frm.doc.tax_id != 'frm.doc.taxid_prefix' ()) {
